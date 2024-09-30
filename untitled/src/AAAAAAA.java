87dc67742cbe73d81a40c5d4f899d441ca4070f5
BryanG2442 <bryangarcia2442@gmail.com>
2024-09-30 14:16:40 -0700

    asdaw

---
+++ untitled/src/AAAAAAA.java
@@ -0,0 +1,12 @@
+public class AAAAAAA {
+    public static void main(String[] args){
+    String movie = "Eiga Daisuki Ponpo-san";
+
+    LikeTheVideo(movie);
+    }
+
+
+    public static void LikeTheVideo (String videoTitle){
+        System.out.println("This person liked the movie " + videoTitle + ".");
+    }
+}
